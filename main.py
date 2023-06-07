18c2c297fa1d9789b41f1dbab9656702fd0c6fa0
SrgMarkov <107784915+SrgMarkov@users.noreply.github.com>
2023-06-07 19:13:30 +0400

    download books from tululu.org

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import os
 import requests
 
-url = 'https://tululu.org/txt.php?id=32168'
-book = requests.get(url)
-print(book.text)
+url = 'https://tululu.org/txt.php?'
+os.makedirs('books', exist_ok=True)
+for book in range(1, 11):
+    book_id = {'id': f'{book}'}
+    book_url = requests.get(url, params=book_id)
+    book_url.raise_for_status()
+
+    with open(f"books/{book_id['id']}.txt", "w") as my_file:
+        my_file.write(book_url.text)
