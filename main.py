4136ffbf48b6377b14374d59267e388d7eb864c1
SrgMarkov <107784915+SrgMarkov@users.noreply.github.com>
2023-06-15 15:02:39 +0400

    Add list comprehension

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def download_image(url, folder='images/'):
     return f"{folder}{picture_name}"
 
 
-def parse_book_page(content):
+def parse_book_page(content, url):
     soup = BeautifulSoup(content.text, 'lxml')
     title_tag = soup.find('h1').text.split('::')
     picture_tag = soup.find('div', class_='bookimage').find('img')['src']
@@ -44,14 +44,9 @@ def parse_book_page(content):
     author = title_tag[1].strip()
     picture_url = urljoin(url, picture_tag)
     comments = soup.find_all('div', class_="texts")
-    comments_text = []
-    for comment in comments:
-        comment_text = comment.text.split(')')[-1]
-        comments_text.append(comment_text)
+    comments_text = [comment.text.split(')')[-1] for comment in comments]
     genres = soup.find('span', class_='d_book').find_all('a')
-    book_genres = []
-    for genre in genres:
-        book_genres.append(genre.text)
+    book_genres = [genre.text for genre in genres]
     book_attributes = {
         'Заголовок': title,
         'Автор': author,
@@ -70,14 +65,14 @@ if __name__ == '__main__':
     command_arguments.add_argument('end_id', help='на каком id закончить загрузку', type=int)
     args = command_arguments.parse_args()
     for book_id in range(args.start_id, args.end_id + 1):
-        url = f'https://tululu.org/b{book_id}/'
+        book_url = f'https://tululu.org/b{book_id}/'
         download_params = {'id': book_id}
         book_txt_url = 'https://tululu.org/txt.php'
-        response = requests.get(url)
-        response.raise_for_status()
+        book_page_response = requests.get(book_url)
+        book_page_response.raise_for_status()
         try:
-            check_for_redirect(response)
-            book_attributes = parse_book_page(response)
+            check_for_redirect(book_page_response)
+            book_attributes = parse_book_page(book_page_response, book_url)
             download_image(book_attributes['Обложка'])
             download_txt(book_txt_url, download_params, f'{book_id}.{book_attributes["Заголовок"]}', folder='books/')
         except requests.TooManyRedirects:
@@ -87,3 +82,4 @@ if __name__ == '__main__':
         except requests.ConnectionError as error:
             print(f'Ошибка сетевого соединения {error}. Перезапуск парсера через 2 минуты')
             time.sleep(120)
+
